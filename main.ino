7eb6c7e0bc375a692c9dfd6fef5d1fbb7870bfe2
juanWilvers <juan.ignacio.wilvers@gmail>
2022-08-22 22:32:29 -0300

    añadi la clase motor, el contrctur y declare un define con los pines, inicialice el constructor

--- main.ino
+++ main.ino
@@ -1,3 +1,40 @@
+#define pin_r 1
+#define pin_l 2
+
+class Motor
+{
+private:
+    int _p_r, _p_l;
+
+public:
+    Motor(int pin_r, int pin_l)
+    {
+        _p_r = pin_r;
+        _p_l = pin_l;
+
+        pinMode(_p_r, OUTPUT);
+        pinMode(_p_l, OUTPUT);
+    }
+    void avanzar(int velocidad)
+    {
+        digitalWrite(_p_r, LOW);
+        analogWrite(_p_l, velocidad);
+    }
+    void retroceder( int velocidad)
+    {
+        digitalWrite(_p_l, LOW);
+        analogWrite(_p_r, velocidad);
+    }
+    void frenar()
+    {
+        digitalWrite(_p_r, LOW);
+        digitalWrite(_p_l, LOW);
+    }
+};
+
+Motor *motor = new Motor(pin_r,pin_l);
+
+
 void setup(){
 
 }
